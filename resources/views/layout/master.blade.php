b91d2c226fc56c04bd3fc8a6a8bac33b65df087c
stephane71183101 <stephane71183101@gmail.com>
2022-06-14 16:59:32 +0200

    layout continuation

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -43,65 +43,176 @@
             </div>
     </header>
     <main>
-    <div class="searchfields">
-        <form>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="row">
-                        <div class="col-6">
+        <div class="searchfields">
+            <form>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="row">
+                            <div class="col-6">
+                                <div class="mb-3 formlabelcolor">
+                                <label for="disabledSelect" class="form-label">TYPE</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>Wein</option>
+                                <option>Schaumwein</option>
+                                <option>Spirituosen</option>
+                                </select>
+                                </div>
+                            </div>
+                            <div class="col-6">
                             <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">TYPE</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>Wein</option>
-                            <option>Schaumwein</option>
-                            <option>Spirituosen</option>
-                            </select>
+                                <label for="disabledSelect" class="form-label">FARBE</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>Rot</option>
+                                <option>Weiss</option>
+                                <option>Rosé</option>
+                                </select>
+                                </div>
+                            </div>
                             </div>
                         </div>
-                        <div class="col-6">
-                        <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">FARBE</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>Rot</option>
-                            <option>Weiss</option>
-                            <option>Rosé</option>
-                            </select>
+                        <div class="col-sm-6">
+                            <div class="row">
+                            <div class="col-6">
+                            <div class="mb-3 formlabelcolor">
+                                <label for="disabledSelect" class="form-label">LAND</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>Land</option>
+                                <option>Land</option>
+                                <option>Land</option>
+                                <option>Land</option>
+                                <option>Land</option>
+                                <option>Land</option>
+                                </select>
+                                </div>
                             </div>
+                            <div class="col-6">
+                            <div class="mb-3 formlabelcolor">
+                                <label for="disabledSelect" class="form-label">WEINREGION</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>Region</option>
+                                <option>Region</option>
+                                <option>Region</option>
+                                <option>Region</option>
+                                <option>Region</option>
+                                <option>Region</option>
+                                </select>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="row">
+                            <div class="col-6">
+                                <div class="mb-3 formlabelcolor">
+                                <label for="disabledSelect" class="form-label">REBSORTE</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>Grenache</option>
+                                <option>Carignan</option>
+                                <option>Merlot</option>
+                                <option>Chardonnay</option>
+                                <option>Syrah</option>
+                                <option>Tempranillo</option>
+                                </select>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                            <div class="mb-3 formlabelcolor">
+                                <label for="disabledSelect" class="form-label">PRODUZENT</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>Produzent</option>
+                                <option>Produzent</option>
+                                <option>Produzent</option>
+                                <option>Produzent</option>
+                                <option>Produzent</option>
+                                </select>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="row">
+                            <div class="col-6">
+                            <div class="mb-3 formlabelcolor">
+                                <label for="disabledSelect" class="form-label">JAHRGANG</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>1996</option>
+                                <option>1999</option>
+                                <option>2001</option>
+                                <option>2006</option>
+                                <option>2011</option>
+                                <option>2018</option>
+                                </select>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                            <div class="mb-3 formlabelcolor">
+                                <label for="disabledSelect" class="form-label">ZUCKERGEHALT</label>
+                                <select id="disabledSelect" class="form-select">
+                                <option>Alle</option>
+                                <option>0-1 g/L</option>
+                                <option>1-2 g/L</option>
+                                <option>2-3 g/L</option>
+                                </select>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        
+        <div class="searchResults">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
+                        </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
-                        <div class="row">
-                        <div class="col-6">
-                        <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">LAND</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>Land</option>
-                            <option>Land</option>
-                            <option>Land</option>
-                            <option>Land</option>
-                            <option>Land</option>
-                            <option>Land</option>
-                            </select>
-                            </div>
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
                         </div>
-                        <div class="col-6">
-                        <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">WEINREGION</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>Region</option>
-                            <option>Region</option>
-                            <option>Region</option>
-                            <option>Region</option>
-                            <option>Region</option>
-                            <option>Region</option>
-                            </select>
-                            </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
+                        </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
                         </div>
                         </div>
                     </div>
@@ -109,72 +220,93 @@
             </div>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-6">
-                        <div class="row">
-                        <div class="col-6">
-                            <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">REBSORTE</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>Grenache</option>
-                            <option>Carignan</option>
-                            <option>Merlot</option>
-                            <option>Chardonnay</option>
-                            <option>Syrah</option>
-                            <option>Tempranillo</option>
-                            </select>
-                            </div>
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
                         </div>
-                        <div class="col-6">
-                        <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">PRODUZENT</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>Produzent</option>
-                            <option>Produzent</option>
-                            <option>Produzent</option>
-                            <option>Produzent</option>
-                            <option>Produzent</option>
-                            </select>
-                            </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
-                        <div class="row">
-                        <div class="col-6">
-                        <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">JAHRGANG</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>1996</option>
-                            <option>1999</option>
-                            <option>2001</option>
-                            <option>2006</option>
-                            <option>2011</option>
-                            <option>2018</option>
-                            </select>
-                            </div>
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
                         </div>
-                        <div class="col-6">
-                        <div class="mb-3 formlabelcolor">
-                            <label for="disabledSelect" class="form-label">ZUCKERGEHALT</label>
-                            <select id="disabledSelect" class="form-select">
-                            <option>Alle</option>
-                            <option>0-1 g/L</option>
-                            <option>1-2 g/L</option>
-                            <option>2-3 g/L</option>
-                            </select>
-                            </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
+                        </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
+                        </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
+                        </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="container">
+                        <div class="prodcutimage">
+                        <img class="img-thumbnail" src="/img/PodrumAleksicNostalgija.jpg">
+                        </div>
+                        <div>
+                        <p class="searchResultline1">Punto Ar Cabernet Franc</p>
+                        </div>
+                        <div>
+                        <p class="searchResultline2">Argentinien | Mendosa</p>
                         </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </form>
-</div>
     </main>
+
     <footer>
     </footer>
 </body>
