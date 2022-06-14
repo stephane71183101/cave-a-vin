1e89998df6ff1eb676004c232be20fbdb8f759f2
stephane71183101 <stephane71183101@gmail.com>
2022-06-14 09:53:45 +0200

    layout first steps

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,8 +8,25 @@
    <title>@yield('title')</title>
 </head>
 <body>
+    <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <a class="headerlogo" href="#">
+                            <img src="/img/cave-a-vin_logo.png" class="rounded float-left" alt="Logo" width="200">
+                        </a>
+                    </div>
+                    <div class="col-9">
+                        <p class="headertext">La cave à vin d'Emma B.</p>
+                    </div>
+                </div>
+            </div>
+    </header>
    <h1>Testen wir Sass und Bootstrap!</h1>
       <div>Tutorial made by Positronx.io</div>   
       <button class="btn btn-warning" type="submit">Bootstrap-Button</button>
+      <br>
+    
+
 </body>
 </html>
